0d3c8ac0ce0da6325d2944b4694cb3d4b0d90322
Matthew Gonzer <matthewgonzer@gmail.com>
2017-10-19 13:51:49 -0600

    add shake gesture

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -20,7 +20,9 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        updateDiceImages()
+       
     }
 
     override func didReceiveMemoryWarning() {
@@ -30,12 +32,24 @@ class ViewController: UIViewController {
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         
+        updateDiceImages()
+        
+    }
+    
+    func updateDiceImages() {
+        
         randomDiceIndex1 = Int(arc4random_uniform(6))
         randomDiceIndex2 = Int(arc4random_uniform(6))
         
         diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
         diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
+        
     }
     
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        
+       updateDiceImages()
+        
+    }
 }
 
