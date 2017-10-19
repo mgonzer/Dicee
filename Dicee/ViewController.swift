02377f21ef8ce0ddbc9f93ecce1047519139893f
Matthew Gonzer <matthewgonzer@gmail.com>
2017-10-18 18:28:53 -0600

    dice rolling ios app

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Matthew Gonzer on 10/18/17.
+//  Copyright © 2017 Matthew Gonzer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+}
+
